71a0bb829e05a40bb92a3e99ba60e8dc85e274c9
haemalgeumi <111968841+haemalgeumi@users.noreply.github.com>
2023-05-09 22:57:25 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,11 +1,12 @@
-function solution(numbers) {
-    let answer = 0;
-    for(let i = 0; i <numbers.length; i++){
-        answer = answer + numbers[i]
-    }
-    return answer / numbers.length;
+// function solution(numbers) {
+//     let answer = 0;
+//     for(let i = 0; i <numbers.length; i++){
+//         answer = answer + numbers[i]
+//     }
+//     return answer / numbers.length;
 
+// }
+function solution(numbers) {
+   return numbers.reduce((acc,cur)=>acc+cur) / numbers.length;
 }
-//각 요소를 다 돌면서 폴문
-//차례대로 더한 다음 
-//갯수만큼 나눈다 numbers.length
+
