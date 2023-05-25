e37c96cf47079f1267db938e668a61348c12877e
haemalgeumi <111968841+haemalgeumi@users.noreply.github.com>
2023-05-25 23:58:36 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -0,0 +1,8 @@
+function solution(strlist) {
+ return strlist.map((el)=>el.length);
+}
+
+    // let answer =[]
+    // for(let i =0; i<strlist.length; i++){
+    //     return answer.push(strlist[i].length);
+    // }
