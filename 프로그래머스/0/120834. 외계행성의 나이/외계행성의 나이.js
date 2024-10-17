7b988ab1429d8f43b9352e12f8a7418d14337058
haemalgeumi <111968841+haemalgeumi@users.noreply.github.com>
2024-10-17 18:05:51 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -0,0 +1,18 @@
+function solution(age) {
+    let answer = ""
+ let stringAge = age.toString();
+    let arr = [...stringAge];
+for(let i = 0; i < stringAge.length; i++){
+    if(arr[i] === "0"){answer += "a"}
+        if(arr[i] === "1"){answer += "b"}
+        if(arr[i] === "2"){answer += "c"}
+        if(arr[i] === "3"){answer += "d"}
+        if(arr[i] === "4"){answer += "e"}
+        if(arr[i] === "5"){answer += "f"}
+        if(arr[i] === "6"){answer += "g"}
+        if(arr[i] === "7"){answer += "h"}
+        if(arr[i] === "8"){answer += "i"}
+     if(arr[i] === "9"){answer += "j"}
+}
+    return answer;
+}
